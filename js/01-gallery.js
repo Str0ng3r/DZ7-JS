318429314f36fbea4998962361988f6194ea084b
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-03-05 18:04:27 +0100

    UP

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,19 +7,20 @@ const divGallery = document.querySelector('.gallery')
 console.log(divGallery)
 
 galleryItems.forEach(({preview,original,description}) => {
-    const divImg = document.createElement('div')
-    divImg.classList.add('gallery__item')
-    const linkImg = document.createElement('a')
-   const imgOne = document.createElement('img');
-   linkImg.classList.add('gallery__link')
-   imgOne.src = preview;
-   imgOne.dataset.source = original;
-   imgOne.alt = description
-   linkImg.href = original
-   imgOne.classList.add('gallery__image')
-   linkImg.appendChild(imgOne)
-   divImg.appendChild(linkImg)
-   divGallery.appendChild(divImg)
+  divGallery.insertAdjacentHTML('beforeend',`<div class="gallery__item"><a href=${original} class="gallery__link"><img class="gallery__image" src=${preview} data-source=${original} alt=${description}></a></div>`)
+  //   const divImg = document.createElement('div')
+  //   divImg.classList.add('gallery__item')
+  //   const linkImg = document.createElement('a')
+  //  const imgOne = document.createElement('img');
+  //  linkImg.classList.add('gallery__link')
+  //  imgOne.src = preview;
+  //  imgOne.dataset.source = original;
+  //  imgOne.alt = description
+  //  linkImg.href = original
+  //  imgOne.classList.add('gallery__image')
+  //  linkImg.appendChild(imgOne)
+  //  divImg.appendChild(linkImg)
+  //  divGallery.appendChild(divImg)
 })
 divGallery.addEventListener('click',(event) => {
     console.log(event.type)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,20 +7,24 @@ const ulGallery = document.querySelector('.gallery')
 console.log(ulGallery)
 
 galleryItems.forEach(({preview,original,description}) => {
-    const liImg = document.createElement('li')
-    liImg.classList.add('gallery__item')
-    const linkImg = document.createElement('a')
-   const imgOne = document.createElement('img');
-   linkImg.classList.add('gallery__link')
-   imgOne.src = preview;
-   imgOne.dataset.source = original;
-   imgOne.alt = description
-   linkImg.href = original
-   imgOne.dataset.caption = description
-   imgOne.classList.add('gallery__image')
-   linkImg.appendChild(imgOne)
-   liImg.appendChild(linkImg)
-   ulGallery.appendChild(liImg)
+    ulGallery.insertAdjacentHTML('beforeend',`<div class="gallery__item"><a href=${original} class="gallery__link"><img class="gallery__image" src=${preview} data-caption=${description} data-source=${original} alt=${description}></a></div>`)
+
+
+
+//     const liImg = document.createElement('li')
+//     liImg.classList.add('gallery__item')
+//     const linkImg = document.createElement('a')
+//    const imgOne = document.createElement('img');
+//    linkImg.classList.add('gallery__link')
+//    imgOne.src = preview;
+//    imgOne.dataset.source = original;
+//    imgOne.alt = description
+//    linkImg.href = original
+//    imgOne.dataset.caption = description
+//    imgOne.classList.add('gallery__image')
+//    linkImg.appendChild(imgOne)
+//    liImg.appendChild(linkImg)
+//    ulGallery.appendChild(liImg)
 })
         let lightbox = new SimpleLightbox('.gallery a', {captions: true,
             captionSelector: 'img',captionsType:'text',captionsData:'alt',captionDelay:250});
